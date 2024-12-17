bc7d0041f4bea2729062879ccac406cb7d0c7543
Adel Nodehi <adelnmo2003@gmail.com>
2024-12-17 04:50:14 +0330

    feat: add an ErrorMessage component

---
+++ src/ui/ErrorMessage.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+
+interface ErrorMessageProp {
+  message: string | undefined;
+}
+
+const ErrorMessage: React.FC<ErrorMessageProp> = ({ message }) => {
+  if (!message) return null;
+
+  return (
+    <div className="flex items-center gap-1.5 rounded bg-red-50 px-2 py-1 text-red-500">
+      <FontAwesomeIcon icon={faTimesCircle} className="text-lg" />
+
+      <p className="text-sm">{message}</p>
+    </div>
+  );
+};
+
+export default ErrorMessage;

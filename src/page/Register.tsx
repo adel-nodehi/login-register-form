4623324b2c63b0a95010100e1692a9b57fcbd631
Adel Nodehi <adelnmo2003@gmail.com>
2024-12-17 04:54:37 +0330

    feat: add RegisterFrom to Register page

--- src/page/Register.tsx
+++ src/page/Register.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import RegisterForm from "../features/authentication/RegisterForm";
 
 const Register: React.FC = () => {
-  return <div>Register</div>;
+  return (
+    <div className="bg-blue-400 min-h-dvh flex justify-center items-center">
+      <RegisterForm />
+    </div>
+  );
 };
 
 export default Register;

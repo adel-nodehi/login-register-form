dcba6711a45f589d2bdd6985e570e68a1cdb0ee0
Adel Nodehi <adelnmo2003@gmail.com>
2024-12-17 04:53:57 +0330

    feat: create register form
    create form and wire up react-hook-form

---
+++ src/features/authentication/RegisterForm.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { registerSchema, registerSchemaType } from "../../models/registerModel";
+import InputField from "../../ui/InputField";
+
+const RegisterForm: React.FC = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<registerSchemaType>({
+    resolver: zodResolver(registerSchema),
+  });
+
+  const onSubmit: SubmitHandler<registerSchemaType> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-80 rounded-lg bg-blue-800 px-2 py-4 text-white shadow-xl"
+      autoComplete="off"
+    >
+      <h2 className="mb-2 text-3xl font-bold tracking-wide">Register</h2>
+
+      <div className="space-y-2">
+        <InputField
+          name="username"
+          type="text"
+          register={register}
+          errorMessage={errors.username?.message}
+        />
+
+        <InputField
+          name="password"
+          type="password"
+          register={register}
+          errorMessage={errors.password?.message}
+        />
+
+        <InputField
+          name="confirmPassword"
+          label="confirm Password"
+          type="password"
+          register={register}
+          errorMessage={errors.confirmPassword?.message}
+        />
+      </div>
+
+      <button className="mt-5 inline-block w-full rounded-lg bg-white px-4 py-2 font-bold tracking-wide text-black">
+        Sign Up
+      </button>
+    </form>
+  );
+};
+
+export default RegisterForm;

66500a4787c139f82ec10fdb98404f30210a9ee9
Adel Nodehi <adelnmo2003@gmail.com>
2024-12-20 22:46:51 +0330

    feat: connect register form to the backend and handle errors

--- frontend/src/features/authentication/RegisterForm.tsx
+++ frontend/src/features/authentication/RegisterForm.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Link } from "react-router";
+import axiosLib from "axios";
 
 import { registerSchema, registerSchemaType } from "../../models/registerModel";
 import InputField from "../../ui/InputField";
 import Button from "../../ui/Button";
-import { Link } from "react-router";
+import axios from "../../api/axios";
 
 const RegisterForm: React.FC = () => {
   const {
@@ -16,8 +18,36 @@ const RegisterForm: React.FC = () => {
     resolver: zodResolver(registerSchema),
   });
 
-  const onSubmit: SubmitHandler<registerSchemaType> = (data) => {
-    console.log(data);
+  const onSubmit: SubmitHandler<registerSchemaType> = async ({
+    username,
+    password,
+  }) => {
+    try {
+      const response = await axios.post(
+        "/register",
+        JSON.stringify({ user: username, pwd: password }),
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        },
+      );
+
+      console.log(response.data);
+      console.log(response);
+      console.log("Success");
+    } catch (err) {
+      if (axiosLib.isAxiosError(err)) {
+        if (!err.response) {
+          console.log("No server Response");
+        } else if (err.response.status === 409) {
+          console.log("Username Taken");
+        } else {
+          console.log("Registration failed");
+        }
+      }
+    }
   };
 
   return (

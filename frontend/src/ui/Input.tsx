7e75b2f0517ce52b0a582d45b7738c919e128920
Adel Nodehi <adelnmo2003@gmail.com>
2024-12-23 23:58:45 +0330

    feat: integrate show/hide password to Input

--- frontend/src/ui/Input.tsx
+++ frontend/src/ui/Input.tsx
@@ -1,4 +1,6 @@
-import { forwardRef, InputHTMLAttributes } from "react";
+import { forwardRef, InputHTMLAttributes, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   className?: string;
@@ -6,12 +8,39 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ ...inputProps }, ref) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    if (inputProps.type === "password")
+      return (
+        <div
+          className={`flex items-center overflow-hidden rounded-md border-b-4 bg-white ${inputProps.className}`}
+        >
+          <input
+            ref={ref}
+            {...inputProps}
+            type={showPassword ? "text" : "password"}
+            className="grow px-2 py-1 text-black outline-none"
+          />
+
+          <span
+            role="button"
+            onClick={() => setShowPassword((prev) => !prev)}
+            className="p-2"
+          >
+            <FontAwesomeIcon
+              className="text-black"
+              icon={showPassword ? faEye : faEyeSlash}
+            />
+          </span>
+        </div>
+      );
+
     return (
       <input
         ref={ref}
         type="text"
         {...inputProps}
-        className={`rounded-md border-b-4 px-2 py-1 text-black outline-none ${inputProps.className}`}
+        className={`rounded-md border-b-4 bg-white px-2 py-1 text-black outline-none ${inputProps.className}`}
       />
     );
   },
